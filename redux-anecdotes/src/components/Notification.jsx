99d7ce50328668dbe4616841faac0a2c394dc3bc
vikram(wsl) <vikramjit360@gmail.com>
2024-08-26 15:43:05 +0530

    6.12 - feat: add state to store for notification component

--- redux-anecdotes/src/components/Notification.jsx
+++ redux-anecdotes/src/components/Notification.jsx
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
+
 const Notification = () => {
+  const notificationText = useSelector(state => state.notification);
+
   const style = {
     border: "solid",
     padding: 10,
     borderWidth: 1,
   };
-  return <div style={style}>render here notification...</div>;
+  return <div style={style}>{notificationText}</div>;
 };
 
 export default Notification;
